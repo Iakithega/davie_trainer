529d8da85cded635cc904822072a728ee5c4c631
Daniil Volf <dvolf@ixto.de>
2024-09-28 19:43:11 +0200

    added multipage

--- 1_Progress.py
+++ 1_Progress.py
@@ -11,25 +11,10 @@ from utils.utils import *
 from utils.data_engineering import *
 from utils.plotting_functions import *
 
-st.set_page_config(
-     page_title="RHEALIZER!",
-     page_icon="🤖", 
-     layout="wide",
-     initial_sidebar_state="expanded",
-     menu_items={
-         'Get Help': 'https://www.bulabula.com/help',
-         'Report a bug': 'https://www.bulabula.com/help',
-         'About': "# We are thinkering with ChatGPT here!"
-     }
- )
 
 
 
-#######################################################################################################################
-import streamlit as st
 
-# original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
-# st.markdown(original_title, unsafe_allow_html=True)
 
 
 # Set the background image
@@ -65,23 +50,29 @@ div[data-baseweb="base-input"] {
 st.markdown(input_style, unsafe_allow_html=True)
 
 
+#######################################################################################################################
+
+# original_title = '<h1 style="font-family: serif; color:white; font-size: 20px;">Streamlit CSS Styling✨ </h1>'
+# st.markdown(original_title, unsafe_allow_html=True)
+
 #############################################################################################################################
 
 
-#remove logo
-hide_st_style = """
-            <style>
-             footer {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)  
+# #remove logo
+# hide_st_style = """
+#             <style>
+#              footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True)  
 
 st.title("David Train")
 
 
+
 start_date = "2024.08.10"
 # Get the current date
-current_date = datetime.today() + timedelta(days=1)
+current_date = datetime.today() + timedelta(days=3)
 # Format the date as 'YYYY-MM-DD'
 current_date = current_date.strftime('%Y-%m-%d')
 
@@ -121,11 +112,11 @@ v_spacer(height=4, sb=False)
 
 
 
-col1_params_normal_plot, col2_params_normal_plot = st.columns([3, 2], gap="large")
-#show here the elements with columns for normal plot
-with col1_params_normal_plot:
-    all_exersices = [column_name for column_name in data.columns]
-    selected_exersices = st.multiselect("Choose exersices", all_exersices)
+# col1_params_normal_plot, col2_params_normal_plot = st.columns([3, 2], gap="large")
+# #show here the elements with columns for normal plot
+# with col1_params_normal_plot:
+#     all_exersices = [column_name for column_name in data.columns]
+#     selected_exersices = st.multiselect("Choose exersices", all_exersices)
 
 
 v_spacer(height=4, sb=False)
@@ -150,42 +141,3 @@ fig_turmzg = turmzg_plot(data, start_date, current_date)
 st.pyplot(fig_turmzg)
 
 
-
-
-# plt.style.use('seaborn-v0_8')
-# fig, axs = plt.subplot_mosaic([
-#                              ['1', '1', '1'],
-#                              ],
-#                              figsize=(11, 3))
-# plt.subplots_adjust(wspace=.2)
-# plt.subplots_adjust(hspace=.6)
-
-# # fig.suptitle(f'''Training''', size=18)
-
-# axs['1'].set_title(f"Progress Weighted Turm Rudern", size=14)
-# axs['1'].set_xlabel(' ', size=14)
-# axs['1'].set_ylabel('Value', size=12)
-# axs['1'].set_xlim([pd.to_datetime("2024.08.10"), pd.to_datetime("2024.09.05")]), # TODAYS DATE 
-# # axs['1'].set_ylim([0, 14])
-
-# # Set font size for major and minor ticks
-# axs['1'].tick_params(axis='x', labelsize=7, rotation=45)  
-# axs['1'].tick_params(axis='x', which='minor', labelsize=7, rotation=45) 
-
-# axs['1'].xaxis.set_major_locator(mdates.WeekdayLocator(byweekday=(TU, WE, TH, FR, SA, SU)))
-# axs['1'].xaxis.set_major_formatter(mdates.DateFormatter('''%d.%m'''))
-# axs['1'].grid(visible=True, which='major', color='grey', axis='x', linestyle='--', linewidth=0.3)
-
-# axs['1'].xaxis.set_minor_locator(mdates.WeekdayLocator(byweekday=(MO)))
-# axs['1'].xaxis.set_minor_formatter(mdates.DateFormatter('''%d.%m''')) # \n %a'
-# axs['1'].grid(visible=True, which='minor', color='black', axis='x', linestyle='--', linewidth=0.3)
-
-# # Plotting the three sets next to each other
-# bar_width = 0.2
-# dates = data.index
-
-# axs['1'].bar(dates - pd.Timedelta(hours=4), data["Weighted Turm Rudern set 1 reps"], alpha=1, width=bar_width, color="limegreen", label="Set 1")
-# axs['1'].bar(dates, data["Weighted Turm Rudern set 2 reps"], alpha=1, width=bar_width, color="dodgerblue", label="Set 2")
-# axs['1'].bar(dates + pd.Timedelta(hours=4), data["Weighted Turm Rudern set 3 reps"], alpha=1, color="darkviolet", width=bar_width, label="Set 3") 
-
-# st.pyplot(fig)
---
+++ streamlit_app.py
@@ -0,0 +1,82 @@
+import streamlit as st
+
+st.set_page_config(
+    #  page_title="RHEALIZER!",
+    #  page_icon="🤖", 
+     layout="wide",
+     initial_sidebar_state="expanded",
+     menu_items={
+         'Get Help': 'https://www.bulabula.com/help',
+         'Report a bug': 'https://www.bulabula.com/help',
+         'About': "# We are thinkering with ChatGPT here!"
+     }
+ )
+
+st.logo(
+    r"media\veeery_lomng.png",
+    link="https://streamlit.io/gallery",
+    icon_image=None,
+)
+
+# #remove logo
+# hide_st_style = """
+#             <style>
+#              footer {visibility: hidden;}
+#             </style>
+#             """
+# st.markdown(hide_st_style, unsafe_allow_html=True) 
+
+# Set the background image
+background_image = """
+<style>
+[data-testid="stAppViewContainer"] > .main {
+    background-image: url("https://images.unsplash.com/photo-1542281286-9e0a16bb7366");
+    background-size: 100vw 100vh;  # This sets the size to cover 100% of the viewport width and height
+    background-position: center;  
+    background-repeat: no-repeat;
+}
+</style>
+"""
+
+st.markdown(background_image, unsafe_allow_html=True)
+
+# st.text_input("", placeholder="Streamlit CSS ")
+
+# input_style = """
+# <style>
+# input[type="text"] {
+#     background-color: transparent;
+#     color: #a19eae;  // This changes the text color inside the input box
+# }
+# div[data-baseweb="base-input"] {
+#     background-color: transparent !important;
+# }
+# [data-testid="stAppViewContainer"] {
+#     background-color: transparent !important;
+# }
+# </style>
+# """
+# st.markdown(input_style, unsafe_allow_html=True)
+
+
+# Define the main navigation
+pg0 = st.Page(r"streamlit_app.py", title="Start", default=True, icon=":material/home:")
+pg1 = st.Page(r"1_Progress.py", title="Progress", default=False, icon=":material/bar_chart_4_bars:")
+pg2 = st.Page(r"views\3_Rewards.py", title="Rewards", icon=":material/emoji_events:")
+pg3 = st.Page(r"views\2_Stories.py", title="Strories", icon=":material/sword_rose:")
+
+
+pg = st.navigation({
+    "Overview": [pg0, pg1, pg2],
+    "Adventure": [pg3],
+})
+
+try:
+    pg.run()
+except Exception as e:
+    st.error(f"Something went wrong: {str(e)}", icon=":material/error:")
+
+
+
+
+
