a601cb95ff94cb42c5744ab7736a7d714d764f7f
Daniil Volf <danjavolf@gmail.com>
2024-10-27 16:11:25 +0100

    nice functionality without removing selected creatures for the rewards game

--- views/5_Rewards_game.py
+++ views/5_Rewards_game.py
@@ -17,20 +17,22 @@ hide_st_style ="""
 st.markdown(hide_st_style, unsafe_allow_html=True)  
 
 st.write("# The Game")
+# v_spacer(height=5, sb=False)
 
 
 config_path = os.path.join("utils", "paths.ini")
 creature_images_path = os.path.join("media", "pics_of_creatures")
-creaturealready_won_images_path = os.path.join("media", "pics_of_creatures", "already_won_creatures")
+creature_already_won_images_path = os.path.join("media", "pics_of_creatures", "already_won_creatures")
+creature_question_mark_image_path = os.path.join("media", "pics_of_creatures", "backup", "creature_question_mark.png")
 
 
 # Initialize session state for controlling the game and storing last images
 if 'start' not in st.session_state:
     st.session_state.start = False
 if 'last_image' not in st.session_state:
-    st.session_state.last_image = None
+    st.session_state.last_image = creature_question_mark_image_path  # Set question mark as the default initial image
 if 'last_image_name' not in st.session_state:
-    st.session_state.last_image_name = None
+    st.session_state.last_image_name = "???"  # Default name for question mark
 
 
 @st.cache_data
@@ -47,9 +49,21 @@ def load_image_paths(creature_images_path):
 # Load and cache image paths
 creature_image_paths = load_image_paths(creature_images_path)
 
-# Start and Stop buttons
-start_button = st.button("Start")
-stop_button = st.button("Stop")
+
+col_start_button, col_image, col_stop_button = st.columns([1,2,1], gap="large", vertical_alignment="center")
+
+with col_image:
+    # placeholder_image_text = st.empty()
+    col_inside_img_start, col_inside_img_gap, col_inside_img_stop = st.columns([15, 70, 10], gap="large", vertical_alignment="center")
+    
+    # Start and Stop buttons
+    with col_inside_img_start:
+        start_button = st.button("Start")
+    with col_inside_img_stop:
+        stop_button = st.button("Stop")
+    placeholder_image = st.empty()  # Placeholder for the image display
+    placeholder_image_text = st.empty()
+        
 
 # Update session state based on button clicks
 if start_button:
@@ -58,8 +72,7 @@ if stop_button:
     st.session_state.start = False
 
 
-# Display area
-placeholder = st.empty()  # Placeholder for the image display
+
 
 # Spinning through random images
 if st.session_state.start:
@@ -68,24 +81,23 @@ if st.session_state.start:
         name = random.choice(list(creature_image_paths.keys()))
         
         # Display image and name in the placeholder
-        placeholder.image(creature_image_paths[name], use_column_width='auto')
-        st.markdown(f"{name.upper()}")
+        placeholder_image.image(creature_image_paths[name], use_column_width='auto')
         
         # Store the last displayed image in session state
         st.session_state.last_image = creature_image_paths[name]
         st.session_state.last_image_name = name
         
-        time.sleep(0.2)  # Control the speed of spinning
+        time.sleep(0.1)  # Control the speed of spinning
         
         # Stop spinning if "Stop" button is pressed
         if not st.session_state.start:
             break
 
 
-# Display the last image shown when stopped
-if not st.session_state.start and st.session_state.last_image:
-    placeholder.image(st.session_state.last_image, use_column_width='auto')
-    st.markdown(f"{st.session_state.last_image_name.upper()}")
+# Display the question mark image if the game has not started, or show the last image when stopped
+if not st.session_state.start:
+    placeholder_image.image(st.session_state.last_image, use_column_width='auto')
+    placeholder_image_text.markdown(f"## {st.session_state.last_image_name.upper()}")
 
 
 
@@ -99,119 +111,6 @@ if not st.session_state.start and st.session_state.last_image:
 
 
 
-
-
-
-
-
-# # Initialize session state for controlling the game and storing last images
-# if 'start' not in st.session_state:
-#     st.session_state.start = False
-# if 'last_images' not in st.session_state:
-#     st.session_state.last_images = {}
-# if 'current_tab' not in st.session_state:  
-#     st.session_state.current_tab = 0  
-
-
-
-# @st.cache_data
-# def load_image_paths(creature_images_path=creature_images_path):
-#     # Get all image files in the media folder (assuming .jpg and .png formats)
-#     image_paths = {}
-#     for filename in os.listdir(creature_images_path):
-#         if filename.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
-#             # Use the filename as the key and the full path as the value
-#             key = os.path.splitext(filename)[0]
-#             full_path = os.path.join(creature_images_path, filename)
-#             image_paths[key] = full_path
-    
-#     return image_paths
-
-# # Bildpfade laden und cachen
-# creature_image_paths = load_image_paths(creature_images_path)
-
-
-
-# # Start and Stop buttons
-# start_button = st.button("Start")
-# stop_button = st.button("Stop")
-
-# # # Update session state based on button clicks
-# if start_button:
-#     st.session_state.start = True
-#     st.session_state.current_tab = 0
-# if stop_button:
-#     st.session_state.start = False
-
-
-# # Random order of images
-# image_names = list(creature_image_paths.keys())
-# random.shuffle(image_names)
-
-
-
-# # Set up dynamic tabs for each creature
-# tab_names = list(creature_image_paths.keys())
-
-# col1, col2, col3 = st.columns([1,2,1], gap="large")
-# with col2:
-#     tabs = st.tabs([name.upper() for name in tab_names])
-
-
-# # Loop through each tab and each image, displaying images if "Start" is active
-# if st.session_state.start:
-#     for name in image_names:
-#         # NEW: Select the current tab based on the current index
-#         current_tab_name = tab_names[st.session_state.current_tab]
-#         with tabs[st.session_state.current_tab]:  # Display in the active tab
-#             placeholder = st.empty()  # Placeholder for the image
-#             # Display image and name in the current tab
-#             placeholder.image(creature_image_paths[name], use_column_width='auto')
-#             st.markdown(f"{name.upper()}")
-            
-#             # Store the last displayed image for each tab in session state
-#             st.session_state.last_images[current_tab_name] = creature_image_paths[name]
-
-#             # Wait for a short period before switching to the next tab
-#             time.sleep(0.2)
-
-#             # NEW: Update to the next tab, looping back to the first tab if necessary
-#             st.session_state.current_tab = (st.session_state.current_tab + 1) % len(tab_names)
-
-#             # Break if stopped
-#             if not st.session_state.start:
-#                 break
-# else:
-#     # Show the last image displayed for each tab after stopping
-#     for idx, tab_name in enumerate(tab_names):
-#         with tabs[idx]:
-#             placeholder = st.empty()
-#             if tab_name in st.session_state.last_images:
-#                 placeholder.image(st.session_state.last_images[tab_name], use_column_width='auto')
-#                 st.markdown(f"{tab_name.upper()}")
-                
-# # Loop through each tab, displaying images if "Start" is active
-# for idx, tab_name in enumerate(tab_names):
-#     with tabs[idx]:
-#         with col2:
-#             placeholder = st.empty()  # Placeholder for the image
-#             if st.session_state.start:
-#                 for name in image_names:
-#                     # Display image and name in the current tab
-#                     placeholder.image(creature_image_paths[name], use_column_width='auto')
-#                     st.markdown(f"{name.upper()}")
-                    
-#                     # Store the last displayed image for each tab in session state
-#                     st.session_state.last_images[tab_name] = creature_image_paths[name]
-                    
-#                     time.sleep(0.2)  # Control the speed of spinning
-#                     if not st.session_state.start:
-#                         break
-#             else:
-#                 # Show the last image displayed when spinning stopped
-#                 if tab_name in st.session_state.last_images:
-#                     placeholder.image(st.session_state.last_images[tab_name], use_column_width='auto')
-#                     st.markdown(f"{tab_name.upper()}")
 
 
 
