e57507ec3e86c9c113a08cf4bb7fa0e9afb2702f
Daniil Volf <danjavolf@gmail.com>
2024-10-27 09:39:13 +0100

    added pics for prices

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,15 +37,16 @@ st.logo(
 # Define the main navigation
 pg0 = st.Page(r"streamlit_app.py", title=" ", default=False, icon=None, url_path=None) # icon=":material/home:" url_path=None, default=False # DAVID THE TITAN
 pg1 = st.Page(r"views\1_Progress.py", title="Progress", default=True, icon=":material/bar_chart_4_bars:")
-pg2 = st.Page(r"views\3_Rewards.py", title="Rewards", icon=":material/emoji_events:")
+pg2 = st.Page(r"views\3_Records.py", title="Records", icon=":material/emoji_events:")
 pg3 = st.Page(r"views\2_Stories.py", title="Strories", icon=":material/sword_rose:")
-pg4 = st.Page(r"views\3_dialogue_with_the_master.py", title="Master", icon=":material/self_improvement:")
+pg4 = st.Page(r"views\5_Rewards_game.py", title="Rewards Game", icon=":material/casino:")
+pg5 = st.Page(r"views\3_dialogue_with_the_master.py", title="Master", icon=":material/self_improvement:")
 pgz = st.Page(r"views\zz_css_stuff.py", title="css_etc", default=False, icon=None, url_path=None) # icon=":material/sword_rose:"
 
 
 pg = st.navigation({
     "Overview": [pg0, pg1, pg2],
-    "Adventure": [pg3, pg4],
+    "Adventure": [pg3, pg4, pg5],
     "Else": [pgz]
 })
 
---
+++ views/5_Rewards_game.py
@@ -0,0 +1,30 @@
+import streamlit as st
+from utils.utils import *
+from utils.data_engineering import complete_data_wrangeling
+from utils.plotting_functions import rec_overview_plot
+
+# hides the header - also possible to hide the footer: footer {visibility: hidden;}  /* Hides the footer */
+hide_st_style ="""
+    <style>
+        header.st-emotion-cache-1n4a2v9 {visibility: hidden;}  /* Hides the Streamlit header element identified in the console */ 
+    </style>
+    """
+st.markdown(hide_st_style, unsafe_allow_html=True)  
+
+st.write("# The Game")
+
+@st.cache_data
+def load_and_process_data():
+    initial_data = load_raw_excel(path_to_excel)
+    data, monthly_stats_data = complete_data_wrangeling(initial_data)
+    return data, monthly_stats_data
+
+# Call the function to cache the data
+data, monthly_stats_data = load_and_process_data() 
+
+# overview of the datas in a dataframe after Data Wrangeling 
+with st.expander("Raw Table monthly stats"): 
+    st.dataframe(monthly_stats_data)
+v_spacer(height=7, sb=False)
+
+
